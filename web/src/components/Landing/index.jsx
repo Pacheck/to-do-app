ef443ded4ccfaab3f2334dbdb602ae3c738edc96
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-11-13 15:16:58 -0300

    created landing component

---
+++ web/src/components/Landing/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './index.css'
+
+const Landing = () => {
+    return (
+        <div>
+            <h1>Hello Lading!</h1>
+        </div>
+    )
+}
+
+export default Landing;
