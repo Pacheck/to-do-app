c6e31d7c60e82eedaa7901387bed8173ad0efa1f
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-11-13 15:16:05 -0300

    created nav component

---
+++ web/src/components/Nav/index.jsx
@@ -0,0 +1,13 @@
+import Reeact from 'react';
+
+import './index.css'
+
+const Nav = () => {
+    return (
+        <nav className="navigator">
+            <h3>Navigator</h3>
+        </nav>
+    )
+}
+
+export default Nav;
