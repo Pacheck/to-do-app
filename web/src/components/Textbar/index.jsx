3c0fa61da057f73a82c6dc15f0af7f7a2a66c84b
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-11-14 19:12:42 -0300

    ads

---
+++ web/src/components/Textbar/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './index.css'
+
+const Textbar = () => {
+    return(
+        <input type="text" placeholder="Adicionar uma tarefa"/>
+    )
+}
+
+export default Textbar;
