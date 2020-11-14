ab1087d2266238bc92aefad322bfad595d0d96f4
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-11-14 19:12:26 -0300

    landing component

--- web/src/components/Landing/index.jsx
+++ web/src/components/Landing/index.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import './index.css'
 
+import Textbar from '../Textbar'
+
 const Landing = () => {
     return (
-        <div>
+        <div className="landing">
             <h1>Hello Lading!</h1>
+            <Textbar />
         </div>
     )
 }
