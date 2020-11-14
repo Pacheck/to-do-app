fc9773107a19f51a9e2f273bef9a71393f7c1090
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-11-14 19:12:33 -0300

    finished component

---
+++ web/src/components/Finished/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './index.css'
+
+const Finished = () => {
+    return (
+        <div className="finished">
+            <h2>Finished!</h2>
+        </div>
+    )
+}
+
+export default Finished
