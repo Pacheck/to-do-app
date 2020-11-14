7f8e91f54901b0d09621d60ab3bea4309b9572d9
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-11-14 19:12:12 -0300

    starred component

---
+++ web/src/components/Starred/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './index.css'
+
+import Textbar from '../Textbar'
+
+const Starred = () => {
+    return (
+        <div className="starred">
+            <h2>Hello, starred!</h2>
+            <Textbar />
+        </div>
+    )
+}
+
+export default Starred
