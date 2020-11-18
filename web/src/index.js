315e4005d6e71b7d039fcb60cf3166d14651799b
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-11-18 20:26:07 -0300

    initialized reducer

--- web/src/index.js
+++ web/src/index.js
@@ -4,10 +4,28 @@ import './index.css';
 import App from './components/App/App';
 
 import { Provider } from 'react-redux';
+import { combineReducers, createStore } from 'redux';
+
+import todosReducer from './redux/reducer/todos';
+
+const rootReducer = combineReducers({
+  landingTodos: todosReducer('Landing'),
+  starredTodos: todosReducer('Starred'),
+  finishedTodos: todosReducer('Finished'),
+});
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+console.log(store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
