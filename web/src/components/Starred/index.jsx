d033d0db933bda1660f9e81ad5051742c404aa65
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-11-21 20:43:58 -0300

    Redux logic abstracted from Starred component to Task and Form component

--- web/src/components/Starred/index.jsx
+++ web/src/components/Starred/index.jsx
@@ -2,13 +2,39 @@ import React from 'react';
 
 import './index.css'
 
-import Textbar from '../Textbar'
+import { useDispatch, useSelector } from 'react-redux';
+
+import { add, edit, del } from "../../redux/actions";
+
+import Form from '../Form';
+import Task from '../Task';
 
 const Starred = () => {
+
+    const starredReducer = useSelector(state => state.starredTodos);
+
+    const Starred = 'Starred';
+
+    const dispatch = useDispatch();
+
     return (
         <div className="starred">
-            <h2>Hello, starred!</h2>
-            <Textbar />
+            
+            <ul>
+                {starredReducer.map((task, index) => {
+                    return (
+                        <li key={index}>
+                            <Task 
+                                reducerType={Starred}
+                                task={task} 
+                                index={index} 
+                            />
+                        </li>
+                    )
+                })}
+           </ul>
+
+            <Form reducerType={Starred} />
         </div>
     )
 }
