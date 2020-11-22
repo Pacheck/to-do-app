7adcae52102a9a18f832df6feeccb0dedc823b1a
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-11-22 16:33:50 -0300

    created a way to do rollback in the tasks that came from another pages

--- web/src/components/Finished/index.jsx
+++ web/src/components/Finished/index.jsx
@@ -2,10 +2,31 @@ import React from 'react';
 
 import './index.css'
 
+import { useSelector, useDispatch } from 'react-redux';
+
+import { add } from '../../redux/actions'
+
 const Finished = () => {
+
+    const finishedReducer = useSelector(state => state.finishedTodos );
+
+
+    const dispatch = useDispatch(); 
+
+    const handleUncheckTask = (task) => {
+        dispatch(add(task.taskOwner, task.payload))
+    }
+
+    console.log(finishedReducer)
+
     return (
         <div className="finished">
-            <h2>Finished!</h2>
+           {finishedReducer.map((task, index) => (
+               <div key={index}>
+                   <input type="checkbox" defaultChecked onClick={() => handleUncheckTask(task)}/>
+                   <h2>{task.payload}</h2>
+               </div>
+           ))}
         </div>
     )
 }
