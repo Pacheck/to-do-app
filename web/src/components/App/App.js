b7b85631652dec2b6ca050fae9041ea46a23051f
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-11-23 17:16:43 -0300

    removed old css adapted new categories nav

--- web/src/components/App/App.js
+++ web/src/components/App/App.js
@@ -1,25 +1,23 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import './App.css';
-
 //Importing components
-import Nav from '../Nav';
-import Landing from '../Landing';
+import Categories from '../Categories';
+import Personal from '../Personal';
 import Starred from '../Starred';
 import Finished from '../Finished';
+
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Nav />
-        <div className="container">
-          <Switch>
-            <Route path="/" exact component={Landing} />
-            <Route path="/importante" component={Starred} />
-            <Route path="/finalizados" component={Finished} />
-          </Switch>
-        </div>
+        <Switch>
+          <Route path="/" exact component={Categories} />
+          <Route path="/personal" component={Personal} />
+          <Route path="/importante" component={Starred} />
+          <Route path="/finalizados" component={Finished} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
