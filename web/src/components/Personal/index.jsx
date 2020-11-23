b1126917f42a5422374929c3ecf0adbf54806271
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-11-23 17:14:29 -0300

    Changed Landing to Personal

---
+++ web/src/components/Personal/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { useHistory } from "react-router-dom"
+import { BsArrowLeft } from 'react-icons/bs'
+import { useSelector } from 'react-redux';
+
+import Form from '../Form';
+import Task from '../Task'
+
+
+import './index.css'
+
+const Personal = () => {
+    const history = useHistory();
+    const Personal = 'Personal'
+
+    const landingReducer = useSelector(state => state.landingTodos);
+
+    const handleNavigateHome = () => {
+        history.push('/');
+    }
+
+    return (
+        <div className="personal">
+
+            <BsArrowLeft onClick={handleNavigateHome}/>
+            
+           <ul>
+                {landingReducer.map(task => {
+                    return (
+                        <li key={task.id}>
+                            <Task
+                                reducerType={Personal}
+                                task={task}  
+                            />
+                        </li>
+                    )
+                })}
+           </ul>
+
+            <Form reducerType={Personal} />
+
+        </div>
+    )
+}
+
+export default Personal;
