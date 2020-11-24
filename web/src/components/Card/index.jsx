d16e723af7aa27248e2869befe46b604733e47f7
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-11-24 17:50:03 -0300

    created reducer length as children of card

--- web/src/components/Card/index.jsx
+++ web/src/components/Card/index.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import './index.css'
 
-const Card = ({ name, children }) => {
+const Card = ({ name, children, reducer }) => {
+
+
     return <div className="card">
         {children}
-        <h2>{name}</h2>
+        <h3>{name}</h3>
+        <p>{reducer.length} tasks</p>
     </div>
 }
 
