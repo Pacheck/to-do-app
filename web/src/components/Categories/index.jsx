e226545d2273c8cf0f400ccb6e70c0e5b8cac521
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-11-24 17:49:44 -0300

    added the reducer to the card params to access the length property

--- web/src/components/Categories/index.jsx
+++ web/src/components/Categories/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
 import { Link } from "react-router-dom";
 import { IoMdPerson } from 'react-icons/io'
 import { TiStarFullOutline } from "react-icons/ti";
@@ -10,6 +11,13 @@ import Card from '../Card';
 import './index.css';
 
 const Categories = () => {
+
+    const personalReducer = useSelector(state => state.personalTodos);
+    const starredReducer = useSelector(state => state.starredTodos);
+    const finishedReducer = useSelector(state => state.finishedTodos);
+
+    console.log(personalReducer)
+
     return (      
             <nav className="navigator">
                 <header>
@@ -17,18 +25,18 @@ const Categories = () => {
                 </header>
                 
                 <Link to="/personal">
-                    <Card name='Personal'>
-                        <IoMdPerson />
+                    <Card name='Personal' reducer={personalReducer}>
+                        <IoMdPerson size={25} className='personal'/>
                     </Card>
                 </Link>
                 <Link to="/importante">
-                    <Card name='Starred'>
-                        <TiStarFullOutline />
+                    <Card name='Starred' reducer={starredReducer}>
+                        <TiStarFullOutline size={25} className='starred'/>
                     </Card>
                 </Link>
                 <Link to="/finalizados">
-                    <Card name='Finished'>
-                        <BiTask />
+                    <Card name='Finished' reducer={finishedReducer}>
+                        <BiTask size={25} className='finished'/>
                     </Card>
                 </Link>        
             </nav>
