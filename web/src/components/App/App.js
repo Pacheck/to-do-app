da90bb770b458f057b14a27ecbc9cfa1f958d206
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-11-29 09:55:52 -0300

    added some css, removed Route-browser component, to Components as a route

--- web/src/components/App/App.js
+++ web/src/components/App/App.js
@@ -1,22 +1,40 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 //Importing components
 import Categories from '../Categories';
-import Personal from '../Personal';
-import Starred from '../Starred';
+// import Personal from '../Personal';
+// import Starred from '../Starred';
 import Finished from '../Finished';
+import Category from '../Category';
+import NotFound from '../NotFound';
 
 import './App.css';
 
 function App() {
+  const testeReducer = useSelector((state) => state.testeTodos);
+
+  console.log(testeReducer);
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact component={Categories} />
-          <Route path="/personal" component={Personal} />
-          <Route path="/importante" component={Starred} />
+          <Categories path="/" exact categories={testeReducer} />
+
+          {testeReducer.map((category) => {
+            return (
+              <Category
+                key={category.name}
+                path={`/${category.name}`}
+                tasks={category.tasks}
+                reducerType={category.name}
+              />
+            );
+          })}
+
           <Route path="/finalizados" component={Finished} />
+
+          <Route path="*" component={NotFound} />
         </Switch>
       </BrowserRouter>
     </div>
