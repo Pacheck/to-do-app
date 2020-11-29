6dc2ba44a8405f4bec4480509dfb9fb71c9b2644
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-11-29 20:14:15 -0300

    reducerType changed to name, add, finished implemented

--- web/src/components/Task/index.jsx
+++ web/src/components/Task/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { edit, del, finish } from '../../redux/actions';
+import { add, edit, del, finish } from '../../redux/actions';
 
 import './index.css'
 
@@ -8,63 +8,72 @@ import { MdEdit } from 'react-icons/md'
 import { ImCheckmark2 } from 'react-icons/im'
 import { AiOutlineDelete } from 'react-icons/ai'
 
-const Task = ({ reducerType, task }) => {
+const Task = ({ task, categoryName }) => {
 
     const [onEdit, setOnEdit] = useState(false);
     const [newInputValue, setNewInputValue] = useState('');
 
+    const FINISHED = 'finished';
+
     const dispatch = useDispatch();
 
-    const { id, categoryTask, text } = task;
+    const payload = { ...task }
 
     const handleFinishTask = () => {
-        const payload = {
-            id, // id
-            text, // text
-            categoryTask, //categoryTask
-        }
-
-        dispatch(finish('Finished', payload));
-
+        const payload = { ...task, }
+        dispatch(finish(payload)); 
         handleDeleteTask();
-
         console.log({ type: 'dispatch/finish', value: task})
+    }
+
+    const handleUncheckTask = () => {   
+        dispatch(add(payload))
+        dispatch(del({...payload, categoryTask: 'finished'}))
+        console.log({ type: 'dispatch/uncheck', value: task})
     }
 
     const handleEditTask = () => {
         setOnEdit(false)
-        const payload = {
+        const newPayload = {
             ...task,
             text: newInputValue
         }
-        dispatch(edit(reducerType, payload))
+        dispatch(edit(newPayload))
         console.log({ type: 'dispatch/edit', value: newInputValue})
     }
 
     const handleDeleteTask = () => {
-        dispatch(del(reducerType, id));
+        console.log(payload)
+        dispatch(del(payload));
+        console.log({ type: 'dispatch/delete', value: task.id})
     }
 
     return (
-        onEdit 
+        <div className="task-container">
+            
+            {onEdit 
             ? 
-        <div>
-            <input 
-                autoFocus
-                defaultValue={task.text}    
-                onChange={(e) => setNewInputValue(e.target.value)}
-                onBlur={handleEditTask}
-            /> 
-            <ImCheckmark2 onClick={handleEditTask}/>
-        </div>
+                <div>
+                    <input 
+                    autoFocus
+                    defaultValue={task.text}    
+                    onChange={(e) => setNewInputValue(e.target.value)}
+                    onBlur={handleEditTask}
+                    /> 
+                    <ImCheckmark2 onClick={handleEditTask}/>
+                </div>
             :
-        <div className="task">
-            <input type="checkbox" onClick={handleFinishTask} />
-            <h3>{task.text}</h3>
-            <div className="task-actions">
-                <MdEdit onClick={() => setOnEdit(true)} />
-                <AiOutlineDelete onClick={handleDeleteTask}/>
-            </div>
+                <div className="task">
+                    <input type="checkbox" 
+                        defaultChecked={categoryName === FINISHED}
+                        onClick={categoryName === FINISHED ? handleUncheckTask : handleFinishTask} 
+                    />
+                    <h3>{task.text}</h3>
+                    <div className="task-actions">
+                        <MdEdit onClick={() => setOnEdit(true)} />
+                        <AiOutlineDelete onClick={handleDeleteTask}/>
+                    </div>
+                </div>}
         </div>
     )        
 
