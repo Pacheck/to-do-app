0145e6908f2660ef879502db60ef7393510bb37a
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-11-30 17:55:37 -0300

    commenting

--- web/src/components/App/App.js
+++ web/src/components/App/App.js
@@ -7,10 +7,27 @@ import Finished from '../Finished';
 import Category from '../Category';
 import NotFound from '../NotFound';
 
+import { IoMdPerson } from 'react-icons/io';
+// import { BiTask } from 'react-icons/bi';
+// import { TiStarFullOutline } from 'react-icons/ti';
+// import { HiStar } from 'react-icons/hi';
+
 import './App.css';
 
 function App() {
+  // const dispatch = useDispatch();
+
+  // dispatch(
+  //   create_category({
+  //     name: 'Personal',
+  //     icon: <IoMdPerson />,
+  //     color: '#257bcc',
+  //     tasks: [{ id: 'ebda@ea', text: 'Estudar!', categoryTask: 'Personal' }],
+  //   })
+  // );
+
   const testeReducer = useSelector((state) => state.testeTodos);
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -24,6 +41,8 @@ function App() {
                 path={`/${category.name}`}
                 tasks={category.tasks}
                 categoryName={category.name}
+                icon={<IoMdPerson size={23} />}
+                color={category.color}
               />
             );
           })}
