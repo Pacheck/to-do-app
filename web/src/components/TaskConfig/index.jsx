2f03f88c49f5ccc780bf7abdf19083385699f447
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-11-30 17:56:44 -0300

    created taskConfig and it css

---
+++ web/src/components/TaskConfig/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { BiDotsVerticalRounded } from 'react-icons/bi'
+
+import './index.css'
+
+const TaskConfig = () => {
+    return <div className="config-wrapper">
+        <BiDotsVerticalRounded size={25}/>
+        <div className="dropdown">
+            <h3>glub</h3>
+            <h3>gleb</h3>
+            <h3>cuck</h3>
+        </div>
+    </div>
+}
+export default TaskConfig;
