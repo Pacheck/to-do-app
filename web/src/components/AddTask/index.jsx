3fa3d575146b8065301659bf4deffdfb03f45e0e
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-12-01 23:46:39 -0300

    AddTask added to lead with create form

---
+++ web/src/components/AddTask/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Task = styled.div`
+    background-color: '#23034d';
+`
+
+const AddTask = ({ categoryName }) => {
+    return (
+        <Task className="SDKFIODFJGPODFJKGOP´DFJGPOJDFG">
+            {categoryName}
+        </Task>
+    )
+}
+
+export default AddTask
