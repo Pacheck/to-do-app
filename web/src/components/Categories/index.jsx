f7e901a21d2833df65179bb31f594f8af9ddc962
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-12-01 23:48:03 -0300

    added AddButton and removed some forms

--- web/src/components/Categories/index.jsx
+++ web/src/components/Categories/index.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
 
 import { Link } from "react-router-dom";
-
-import { IoMdPerson } from 'react-icons/io'
-
-import { BiTask } from 'react-icons/bi';
 import { MdSort } from 'react-icons/md'
-
+//Default icon
 
 import Card from '../Card';
+import AddButton from '../AddButton'
 
 import './index.css';
 
 const Categories = ({ categories }) => {
+
+    const ADD_CATEGORY = 'ADD_CATEGORY';
+       
     return (      
             <nav className="navigator">
                 <header>
                     <h2> Lists </h2>
                 </header>
-    
+                
                 {categories.map((category) => (
                     <Link to={`/${category.name}`} key={category.name}>
+                        
                         <Card name={category.name} reducerLength={category.tasks.length}>
-                            <BiTask size={24} />
+                            {category.icon}
                         </Card>
                     </Link>
                 ))}
+
+                <AddButton type={ADD_CATEGORY}/> 
+
             </nav>
     )
 }
