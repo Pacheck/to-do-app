e2ee205e6facace86117665d2f0daf83a38bd446
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-12-07 19:32:52 -0300

    created new form component to use in the future

--- web/src/components/Form/index.jsx
+++ web/src/components/Form/index.jsx
@@ -1,38 +1,65 @@
-import React from 'react';
-import { v4 as uuid_v4 } from "uuid";
-import { useDispatch } from 'react-redux';
+{/* <AddNewCategory showModal={showModal}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <button onClick={() => history.push('/')}>Voltar</button> 
+                    //VAI SER UM SVG PRA VOLTAR
+               
 
-import { add, create_category } from '../../redux/actions';
+                <div className="nav">
+                    <h4 className="nav-title">New Category</h4>
+                    <MdClose size={27} onClick={() => history.push('/')}/>
+                </div>
 
-import './index.css'
+                <div className="category-name">
+                    <label >Category name?</label>
+                    <input  name="name" placeholder="Personal" ref={register({ required: true})} />
+                    {errors.name && <p style={{ color: 'red'}}>This field is required</p>}
+                </div>
+                
+                <div className="category-color">
+                    <label className="label-color">Category color:</label>
+                    <input className="input-color" name="color" type="color" ref={register} defaultValue='#1299D3'/>
+                </div>
 
-const Form = ({ categoryName }) => {
+                <div className="category-icon">
+                    <label>Selecioe um ícone</label>
+                    <button  type="button" onClick={handleModal}>Show Icons</button>
+                </div>
 
-    const dispatch = useDispatch();
+                <input className="input-button" type="submit" value="Create"/>
+            </form>
+            
 
-    function handleSubmit(e){
-        e.preventDefault();
-        const value = e.target[0].value;
+            {showModal && 
+                <Modal handleModal={handleModal} showModal={showModal}>
+                    <div className="wrapper">
+                        <div className="icon" onClick={() => handleIcon(FITNESS)}>
+                            <IoMdFitness size={35} />
+                        </div>
+                        <div className="icon" onClick={() => handleIcon(SECRET)}>
+                            <IoMdLock size={35} />
+                        </div>
+                        <div className="icon" onClick={() => handleIcon(PERSONAL)}>
+                            <IoMdPerson size={35} />
+                        </div>
+                        <div className="icon" onClick={() => handleIcon(NOT_SECRET)}>
+                            <IoMdUnlock size={35}/>
+                        </div>
+                        <div className="icon" onClick={() => handleIcon(KEY)}>
+                            <IoIosKey size={35}/>
+                        </div>
+                        <div className="icon" onClick={() => handleIcon(DOCUMENT)}>
+                            <IoIosPaper size={35}/>
+                        </div>
+                        <div className="icon" onClick={() => handleIcon(PAYMENT)}>
+                            <MdPayment size={35} />
+                        </div>
+                        <div className="icon" onClick={() => handleIcon(STAR)}>
+                            <HiStar size={35} />
+                        </div>
 
-        const payload = {
-            id: uuid_v4(),
-            text : value,
-            categoryTask: categoryName
-        }
-        
-        console.log({ type: 'dispatch/add', payload: payload})
-        
-        dispatch(add(payload))
-
-        e.target[0].value = '';
-    }
-
-    return(
-        <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Adicione uma task"/>
-                <button type="submit">Adicionar a tasks</button>
-        </form>
-    )
-}
-
-export default Form;
+                    </div>
+                        <button type="button" onClick={handleModal}>Select</button>
+                </Modal>
+                }
+       
+        </AddNewCategory> */}
